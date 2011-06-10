9e68fb7613309b04d127445407e50802131b1750
Morgawr <morgawr@gmail.com>
2011-06-10 17:52:17 +0200

    :)

---
+++ InterTalk/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterTalk
+{
+    public sealed class Core
+    { 
+        public delegate void CoreEventHandler(object sender, CoreEventArgs e);
+        private Dictionary<String, CoreEventHandler>[] Conditions;
+    }
+}
